3804df0643c89e092c21fb05f9b522c4711c9dee
ElizavetaNoskova <wliza68@gmail.com>
2022-12-16 19:37:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+       // task4();
+       // task5();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        for (int i = 10; i>=1 ; i=i-1) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+
+    }
+}
